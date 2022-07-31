5d668be297c8d41dbf174603da9b5ba9229093c1
phc09188 <phc09188@gmail.com>
2022-07-31 16:56:26 +0900

    07.31 백준 연습

---
+++ src/codeTest/boj2869.java
@@ -0,0 +1,21 @@
+package codeTest;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class boj2869 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+        int sec = A-B;
+        int cnt = (V-A)/sec;
+        if((V-A)%sec != 0) {
+            System.out.println(cnt+2);
+        }else{
+            System.out.println(cnt+1);
+        }
+    }
+}
