0bf39f79710517abda6ba109193904f75b17f996
chandle <phc09188@gmail.com>
2022-11-10 17:10:09 +0900

    18주차 코딩테스트

---
+++ src/BAEKJOON/boj1414.java
@@ -0,0 +1,11 @@
+package BAEKJOON;
+
+import java.io.*;
+import java.util.*;
+public class boj1414 {
+
+    public static void main(String[] args) throws  IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+    }
+}
