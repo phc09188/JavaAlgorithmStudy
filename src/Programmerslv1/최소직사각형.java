0671ec35a79cd484976ad26d9dbda7faf9a4aa2a
chandle <phc09188@gmail.com>
2023-01-07 23:53:15 +0900

    프로그래머스 lv1 문자열문제

---
+++ src/Programmerslv1/최소직사각형.java
@@ -0,0 +1,11 @@
+package Programmerslv1;
+
+public class 최소직사각형 {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        for (int i = 0; i < sizes.length; i++) {
+            int[] arr = sizes[i];
+        }
+        return answer;
+    }
+}
